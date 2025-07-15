106ce12455c7d14eb666160864bf96e16267ba61
mikabrytu <mikabrytu@gmail.com>
2025-07-15 20:33:07 -0300

    Write and read json file with highscore

--- managers/score/score.go
+++ managers/score/score.go
@@ -1,19 +1,73 @@
 package score
 
-var score int
+import (
+	"os"
+	"time"
+
+	savesystem "github.com/mikabrytu/gomes-engine/systems/save"
+)
+
+type Score struct {
+	Score     int       `json:"score"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+var current int
+var high int
+var file string
+var score Score
 
 func Init() {
-	score = 0
+	current = 0
+	file = "highscore.json"
+
+	if !fileExists(file) {
+		createFile(file)
+	}
+
+	err := savesystem.Load(file, &score)
+	if err != nil {
+		panic(err)
+	}
+
+	high = score.Score
 }
 
 func Add(point int) {
-	score += point
+	current += point
 }
 
-func Show() int {
-	return score
+func ShowCurrent() int {
+	return current
+}
+
+func ShowHigh() int {
+	return high
+}
+
+func SaveCurrent() {
+	if current <= high {
+		return
+	}
+
+	high = current
+	newScore := Score{
+		Score:     high,
+		Timestamp: time.Now(),
+	}
+	savesystem.Save(newScore, file)
 }
 
 func Reset() {
-	score = 0
+	current = 0
+}
+
+func createFile(path string) {
+	var empty Score
+	savesystem.Save(empty, path)
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || !os.IsNotExist(err)
 }
